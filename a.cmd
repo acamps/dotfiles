f5a72abab2ab65812afc6fb277fb6a8a782e1227
Albert Camps <me@albertcamps.io>
2014-08-04 11:03:32 +0200

    Create a.cmd
    File that includes windows shortcuts for a cmd loaded with %SystemRoot%\system32\cmd.exe /K C:\utils\a.cmd

---
+++ a.cmd
@@ -0,0 +1,22 @@
+doskey gs=git status
+doskey ga=git add -A
+doskey gc=git commit -m $*
+doskey gm=git commit -m $*
+doskey gp=git push
+doskey ..=cd ..
+doskey jtt=cd C:\Users\acamps\projects\interactive\JiraTicketTracker\trunk\jiratickettracker
+doskey mt=mvn test
+doskey mp=mvn package
+doskey mjd=mvn jboss-as:deploy
+doskey mjr=mvn jboss-as:redeploy
+doskey mju=mvn jboss-as:undeploy
+doskey jk=cd C:\Program Files (x86)\Jenkins
+doskey home=cd %USERPROFILE%
+doskey vw=cd %USERPROFILE%/vimwiki
+doskey jb=cd c:\jboss71\bin
+doskey fwk=cd c:\Users\acamps\Desktop\framework
+
+
+cd %USERPROFILE%
+cls
+
