620bcd37a6e1f2e52eabcec9827a8b45305c4429
Albert Camps <me@albertcamps.io>
2015-03-24 12:21:06 +0100

    Update utilities.

--- utils/autocommiter.sh
+++ utils/autocommiter.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-now="$(date)"
-cd /Users/albertcamps/vimwiki
-/usr/local/bin/git add . -A
-/usr/local/bin/git commit -m "Update knowledge of $now"
-/usr/local/bin/git push
 #To be used with a cron job
+
+repository_commit ()
+{
+    cd $1
+    /usr/local/bin/git pull --no-edit
+    /usr/local/bin/git add . -A
+    /usr/local/bin/git commit -m "$2"
+    /usr/local/bin/git push
+}
+
+now="$(date +\"%y-%m-%d\")"
+echo $now
+repository_commit /Users/albertcamps/vimwiki "Update knowledge of $now"
+repository_commit /Users/albertcamps/Documents/projects/acamps.github.io "Update blog posts of $now"
 
