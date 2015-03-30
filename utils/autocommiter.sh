c3cb0c9b0103c76189b9fbd3eb9b17f1d23e6a75
Albert Camps <me@albertcamps.io>
2015-03-30 18:14:16 +0200

    Add improvements to autocommitter

--- utils/autocommiter.sh
+++ utils/autocommiter.sh
@@ -4,12 +4,14 @@
 repository_commit ()
 {
     cd $1
-    /usr/local/bin/git pull --no-edit
+    /usr/bin/ssh-agent bash -c '/usr/bin/ssh-add /Users/albertcamps/.ssh/id_rsa_3; /usr/local/bin/git pull --no-edit'
     /usr/local/bin/git add . -A
     /usr/local/bin/git commit -m "$2"
-    /usr/local/bin/git push
+    /usr/bin/ssh-agent bash -c '/usr/bin/ssh-add /Users/albertcamps/.ssh/id_rsa_3; /usr/local/bin/git push'
 }
 
+#Start with the correct ssh-agent for accessing the repositories.
+/usr/bin/ssh-agent bash -c '/usr/bin/ssh-add /Users/albertcamps/.ssh/id_rsa_3'
 now="$(date +\"%y-%m-%d\")"
 echo $now
 repository_commit /Users/albertcamps/vimwiki "Update knowledge of $now"
