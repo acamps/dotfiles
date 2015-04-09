df4acd93deb41a9535a1c7263b39d47e25d94e70
Albert Camps <me@albertcamps.io>
2015-04-09 16:32:17 +0200

    Update autocommiter

--- utils/autocommiter.sh
+++ utils/autocommiter.sh
@@ -4,10 +4,10 @@
 repository_commit ()
 {
     cd $1
-    /usr/bin/ssh-agent bash -c '/usr/bin/ssh-add /Users/albertcamps/.ssh/id_rsa_3; /usr/local/bin/git pull --no-edit'
+    /usr/local/bin/git pull --no-edit
     /usr/local/bin/git add . -A
     /usr/local/bin/git commit -m "$2"
-    /usr/bin/ssh-agent bash -c '/usr/bin/ssh-add /Users/albertcamps/.ssh/id_rsa_3; /usr/local/bin/git push'
+    /usr/local/bin/git push
 }
 
 #Start with the correct ssh-agent for accessing the repositories.
